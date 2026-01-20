9a2429dd6259151b5ded6f9aaa589ed9e703e780
Christian Rafael <christianparadix@gmail.com>
2026-01-19 22:06:23 -0300

    feat: Criando a solução e projetos

---
+++ NetCleanArchMvc.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NetCleanArchMvc.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

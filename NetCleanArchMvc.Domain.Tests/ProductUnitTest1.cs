295d12415ea77f2d3a7ca9d74824493eedc6b8d3
Christian Rafael <christianparadix@gmail.com>
2026-01-20 20:48:31 -0300

    feat: Fazendo ajustes no modelo de domínio Product

--- NetCleanArchMvc.Domain.Tests/ProductUnitTest1.cs
+++ NetCleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -56,6 +56,13 @@ public class ProductUnitTest1
     }
 
     [Fact]
+    public void CreateProduct_WithNullImageName_NoNullReferenceException()
+    {
+        Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+        action.Should().NotThrow<NullReferenceException>();
+    }
+
+    [Fact]
     public void CreateProduct_InvalidPriceValue_DomainException()
     {
         Action action = () => new Product(1, "Product Name", "Product Description", -9.99m,

c5f7871e46e52b166ddf7f54b9ebe55c9d8ae99c
Christian Rafael <christianparadix@gmail.com>
2026-01-19 23:42:44 -0300

    feat: Testes de unidade para a entidade Product

---
+++ NetCleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using NetCleanArchMvc.Domain.Entities;
+
+namespace NetCleanArchMvc.Domain.Tests;
+
+public class CategoryUnitTest1
+{
+    [Fact(DisplayName = "Create Category With Valid State")]
+    public void CreateCategory_WithValidParameters_ResultObjectValidState()
+    {
+        Action action = ()=> new Category(1, "Category Name");
+        action.Should()
+            .NotThrow<NetCleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+    }
+
+    [Fact]
+    public void CreateCategory_NegativeIdValue_DomainExceptionInvalidId()
+    {
+        Action action = () => new Category(-1, "Category Name");
+        action.Should()
+            .Throw<NetCleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid Id value");
+    }
+
+    [Fact]
+    public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+    {
+        Action action = () => new Category(1, "Ca");
+        action.Should()
+            .Throw<NetCleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters");
+    }
+
+    [Fact]
+    public void CreateaCategory_MissingNameValue_DomainExceptionRequiredName()
+    {
+        Action action = () => new Category(1, "");
+        action.Should()
+            .Throw<NetCleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+            .WithMessage("Invalid name. Name is required");
+    }
+
+    [Fact]
+    public void CreateCategory_WithNullNameValue_DomainExceptionInvalidName()
+    {
+        Action action = () => new Category(1, null);
+        action.Should()
+            .Throw<NetCleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+    }
+}

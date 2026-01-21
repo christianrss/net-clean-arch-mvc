947ca73ccdefb69d843837db57d0533c6b1f8812
Christian Rafael <christianparadix@gmail.com>
2026-01-20 22:31:03 -0300

    feat: Configurando as entidades usando a Fluent API

--- NetCleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ NetCleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -7,9 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
-    {
-        
-    }
+    { }
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
